444a1e2b98e036cafa7ef0de56583573970b8e23
Felipe Rodrigues Farias <190190f@gmail.com>
2016-10-20 17:08:38 -0200

    Adicionar entity rotas

---
+++ api/src/Model/Entity/Rotas.php
@@ -0,0 +1,75 @@
+<?php
+namespace Model\Entity;
+class Rotas
+{
+    private $id;
+    private $origem;
+    private $destino;
+    private $nome;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrigem()
+    {
+        return $this->origem;
+    }
+
+    /**
+     * @param mixed $origem
+     */
+    public function setOrigem($origem)
+    {
+        $this->origem = $origem;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDestino()
+    {
+        return $this->destino;
+    }
+
+    /**
+     * @param mixed $destino
+     */
+    public function setDestino($destino)
+    {
+        $this->destino = $destino;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+
+}
