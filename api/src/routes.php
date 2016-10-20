4e1f010a12d5a7828b17350b7080e9a8696d003d
Felipe Rodrigues Farias <190190f@gmail.com>
2016-10-20 17:36:53 -0200

    POST - /rotas/add, adicionar rota

--- api/src/routes.php
+++ api/src/routes.php
@@ -1,4 +1,31 @@
 <?php
+use Model\Dao\DaoRotas;
+use Model\Entity\Rotas;
+
+$app->post('/rotas/add', function ($request, $response, $args) {
+    if($request->isPost()){
+        try{
+            $parsedBody = $request->getParsedBody();
+
+            $obj = new Rotas();
+
+            $obj->setOrigem($parsedBody["origem"]);
+            $obj->setDestino($parsedBody["destino"]);
+            $obj->setKm($parsedBody["km"]);
+            $obj->setNome($parsedBody["nome"]);
+
+            $dao = new DaoRotas();
+            return ($dao->add($obj));
+
+        }catch (Exception $e){
+            $data                   = new stdClass();
+            $data->success          = false;
+            $data->error            = true;
+            $data->message          = $e->getMessage();
+            return json_encode($data);
+        }
+    }
+});
 
 $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'inicio.phtml', $args);
