bab0d9c1873eafdc9cac8f490572c38ae29a2916
Felipe Rodrigues Farias <190190f@gmail.com>
2016-10-20 17:19:51 -0200

    adicionar conexao no __construct

--- api/src/Model/Dao/DaoRotas.php
+++ api/src/Model/Dao/DaoRotas.php
@@ -5,10 +5,15 @@ use Model\Entity\Rotas;
 
 class DaoRotas
 {
+    private $cn;
 
     public function __construct()
     {
-
+        try{
+            $this->cn = Connection::getInstance();
+        }catch (\Exception $e){
+            echo $e->getMessage();
+        }
     }
 
 
