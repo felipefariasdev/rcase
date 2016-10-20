dfd3b36e47b28d75845f7eca4a604de17cb85eb8
Felipe Rodrigues Farias <190190f@gmail.com>
2016-10-20 17:38:51 -0200

    ajustar conexao

--- api/src/config_db.php
+++ api/src/config_db.php
@@ -1,13 +1,13 @@
 <?php
 return [
     'db_dev' => [
-        'host' => 'http://mysql.desenvolvedorphprj.com.br/',
+        'host' => 'mysql.desenvolvedorphprj.com.br',
         'user' => 'desenvolvedorp',
         'password' => 'mudar555',
         'name' => 'desenvolvedorp'
     ],
     'db_prod' => [
-        'host' => 'http://mysql.desenvolvedorphprj.com.br/',
+        'host' => 'mysql.desenvolvedorphprj.com.br',
         'user' => 'desenvolvedorp',
         'password' => 'mudar555',
         'name' => 'desenvolvedorp'
