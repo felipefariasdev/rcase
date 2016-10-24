31d031a54362a8a056e5ffa911fdbb5ad7a67a85
Felipe Rodrigues Farias <190190f@gmail.com>
2016-10-24 18:00:00 -0200

    teste de insert em massa via get passando, origem, destino, qtd de linhas inseridas

--- api/src/routes.php
+++ api/src/routes.php
@@ -27,6 +27,42 @@ $app->post('/rotas/add', function ($request, $response, $args) {
     }
 });
 
+$app->get('/rotas/teste/add/{origem}/{destino}/{qtd_insert}', function ($request, $response, $args) {
+
+    $qtd_insert = ($args["qtd_insert"]);
+    $origem = ($args["origem"]);
+    $destino = ($args["destino"]);
+
+    for($i=1;$i<=$qtd_insert;$i++){
+        try{
+
+
+            $km = ($i*10);
+            $nome = "caminhos".$i;
+
+
+            $obj = new Rotas();
+
+            $obj->setOrigem($origem);
+            $obj->setDestino($destino);
+            $obj->setKm($km);
+            $obj->setNome($nome);
+
+            $dao = new DaoRotas();
+            echo $dao->add($obj);
+
+        }catch (Exception $e){
+            $data                   = new stdClass();
+            $data->success          = false;
+            $data->error            = true;
+            $data->message          = $e->getMessage();
+            return json_encode($data);
+        }
+    }
+});
+
+
+
 $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'inicio.phtml', $args);
 });
